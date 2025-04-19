838496cfa194ffacd7733fc0bdafc221c427b643
Urvan <urvan.christen@gmail.com>
2025-04-19 16:09:35 +0200

    STA-9 Import characteristics

---
+++ src/fight/fleets.py
@@ -0,0 +1,43 @@
+# -*-coding:utf-8 -*-
+"""
+@File      :   fleets.py
+@Time      :   2025/04/19 15:02:23
+@Author    :   Urvan Christen
+@Version   :   1.0
+@Contact   :   urvan.christen@gmail.com
+@Desc      :   Module importing and containing the armadas of the game.
+"""
+
+import numpy as np
+import pandas as pd
+
+from utils.paths import data
+from fight.fight import Armada
+
+
+def read_armadas() -> pd.DataFrame:
+    """
+    Reads the armadas from the CSV file and returns a DataFrame.
+    """
+    armada_df = pd.read_csv(data("assets/ships_chars.csv"))
+    armada_array = armada_df[
+        ["attack_value", "shield_power", "shield_power", "structure_points"]
+    ].values.T
+    armada_array[
+        3, :
+    ] //= 10  # Convert structure points to the same scale as the other values
+
+    armada = Armada.from_array(armada_array)
+    return armada
+
+
+def read_ship_names() -> pd.Series:
+    """
+    Reads the ship names from the CSV file and returns a Series.
+    """
+    ships_df = pd.read_csv(data("assets/ships_type_translation.csv"), index_col=1)
+    return ships_df["name"]
+
+
+ARMADA = read_armadas()
+SHIP_NAMES = read_ship_names()
