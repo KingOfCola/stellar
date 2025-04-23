23618deb4552ad6b14f45e3fac240513afdb906b
Urvan <urvan.christen@gmail.com>
2025-04-23 23:59:41 +0200

    STA-23 Extracting graphic assets from resources

--- src/prepare/graphics.py
+++ src/prepare/graphics.py
@@ -15,6 +15,8 @@ import shutil
 import urllib
 from tqdm import tqdm
 
+from PIL import Image
+
 from utils.paths import output, data, path_to_file
 
 
@@ -25,6 +27,7 @@ BACKGROUND_PATTERN = re.compile(
     r"[^\(]*background-image: url\("
     r"\"[/\w\.]+/(?P<url>\w+\.(?:png|jpg))\""
 )
+RESOURCE_PATTERN = re.compile(r"#technologydetails\.(?P<name>\w+)[: ]")
 
 
 def extract_urls_from_css(css: str) -> list[str]:
@@ -99,6 +102,32 @@ def find_backgrounds_mappings(css: str) -> dict[str, str]:
     return mappings
 
 
+def extract_images(
+    filename: str, sizes: dict[str, tuple[int, int, int, int]], outdir: str
+) -> None:
+    """
+    Extracts images from the given file and saves them to the specified directory.
+
+    Parameters
+    ----------
+    filename : str
+        The name of the file to extract images from.
+    shifts : list[int]
+        The list of shifts to apply to the images.
+    """
+    image = Image.open(filename)
+    width, height = image.size
+
+    for name, (x_shift, y_shift, width, height) in sizes.items():
+        x1 = x_shift
+        y1 = y_shift
+        x2 = x1 + width
+        y2 = y1 + height
+
+        cropped_image = image.crop((x1, y1, x2, y2))
+        cropped_image.save(f"{outdir}/{name}.png")
+
+
 if __name__ == "__main__":
     # Paths
     RESOURCE_FILE = data("raw/1bee2581139faeb7d6e395e6c48f91.css")
@@ -121,12 +150,35 @@ if __name__ == "__main__":
     download_urls(urls, DOWNLOAD_DIR)
 
     # Put the wanted images in the images directory
+    constructions_dir = IMAGES_DIR / "constructions"
+    constructions_dir.mkdir(parents=True, exist_ok=True)
+
     for name, url in mappings.items():
         filename = urllib.parse.urlparse(url).path.split("/")[-1]
         output_path = output(f"{DOWNLOAD_DIR}/{filename}").absolute().as_posix()
-        new_path = IMAGES_DIR / f"{name}.png"
+        new_path = constructions_dir / f"{name}.png"
         if os.path.exists(output_path):
             if not os.path.exists(new_path):
                 shutil.copy(output_path, new_path)
         else:
             print(f"File {output_path} does not exist.")
+
+    # Extract resource images from the downloaded files
+    resource_image_filename = "7f14c18b15064d2604c5476f5d10b3.png"
+    resources_sizes = {
+        "metal": (0, 0, 80, 80),
+        "crystal": (80, 0, 80, 80),
+        "deuterium": (160, 0, 80, 80),
+        "energy": (240, 0, 80, 80),
+        "darkmatter": (320, 0, 80, 80),
+        "tritium": (400, 0, 80, 80),
+        "food": (480, 0, 80, 80),
+        "population": (560, 0, 80, 80),
+    }
+
+    resource_image_filename = DOWNLOAD_DIR / resource_image_filename
+    resource_image_filename = resource_image_filename.absolute().as_posix()
+
+    outdir = IMAGES_DIR / "resources"
+    outdir.mkdir(parents=True, exist_ok=True)
+    extract_images(resource_image_filename, resources_sizes, outdir)
